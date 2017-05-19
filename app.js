b76eb2b6c16a92b227eb834c6a814b5d61245a87
iambindu <you@example.com>
2017-05-19 08:35:04 +0530

    already commited

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-var app=angular.module("myApp",[])
-app.controller("myController",["$scope",function($scope){
-$scope.message="Hello Bindu";
 
-$scope.employeeList=[
-		{name:'sunder',age:33,city:'mysore',mobile:'888546921'}
-		{name:'sri',age:45,city:'Hyderabad',mobile:'889756254'}
-		{name:'kanth',age:99,city:'techera',mobile:'954763015'}
-		{name:'reddy',age:100,city:'mysore',mobile:'948630126'}
-]
+var app = angular.module("myApp",['ui.router']);
+
+app.config(["$locationProvider", function($locationProvider){
+	$locationProvider.hashPrefix('')
 }])
+
+app.config(["$stateProvider", function($routeProvider){
+	
+	$stateProvider
+		.state("home",{
+            url:'/',
+			templateUrl: "pages/home.html",
+			controller: "myHomeController",
+            caseInsensitiveMatch:true
+		})
+		.state("register",{
+            url:'/register',
+			templateUrl : "pages/register.html",
+			controller: "myRegisterController",
+            caseInsensitiveMatch:true
+		})
+		.state("login", {
+             url:'/login',
+			templateUrl: "pages/login.html",
+			controller:"myLoginController"
+		})
+}])
+app.controller("myHomeController", ["$scope", function($scope){
+	$scope.message = "This is my home controller";
+}])
+
+app.controller("myRegisterController", ["$scope", function($scope){
+	$scope.message= "This is my register controller";
+}])
+
+app.controller("myLoginController", ["$scope", function($scope){
+	$scope.message = "This is my login controller";
+}])
+
